963d21f78eb3cbba2b7cf08764225124d326b6e7
dspring86 <dspring@equityeng.com>
2016-03-16 15:16:33 -0400

    changed to XMLrequest

--- TestLab/static/js/post-to-server.js
+++ TestLab/static/js/post-to-server.js
@@ -54,7 +54,7 @@
 	    var fileInput = document.getElementById('file-id');
 	    var file = fileInput.files[0];
 	    formData.append("my-file", file);   
-	    
+	    /*
 	    $.ajax({
 	        url: destinationURI,
 	        type: "POST",
@@ -69,14 +69,15 @@
 	        	alert("failed");
 	        }
 	     });
+	    */
 	     
 	    
 	    // Send XMLHttpRequest 
-	    //sendXHRequest(formData, destinationURI);
+	    sendXHRequest(formData, destinationURI);
 	  }
 	}
 	
-	/*
+	
 	// Once the FormData instance is ready and we know
 	// where to send the data
 	function sendXHRequest(formData, uri) {
@@ -134,7 +135,7 @@
 	    result.innerHTML = '<p>The server saw it as:</p><pre>' + evt.target.responseText + '</pre>';
 	  }
 	}
-	*/
+	
 	
 
 }(window, jQuery));
