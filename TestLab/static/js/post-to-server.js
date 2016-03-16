ef446bbd97e33c16cdc309de6276c66edc672b65
dspring86 <dspring@equityeng.com>
2016-03-16 14:58:02 -0400

    testing

--- TestLab/static/js/post-to-server.js
+++ TestLab/static/js/post-to-server.js
@@ -53,8 +53,7 @@
 	    // FAppend the file to FormData - dummy change
 	    var fileInput = document.getElementById('file-id');
 	    var file = fileInput.files[0];
-	    formData.append("my-file", file);
-	    document.write(formData);    
+	    formData.append("my-file", file);   
 	    
 	    $.ajax({
 	        url: destinationURI,
