cc54bdc9d180d98846d97265cb67b35aaa6fcb61
dspring86 <dspring@equityeng.com>
2016-03-16 08:32:39 -0400

    made post-to-server script global

--- TestLab/static/js/post-to-server.js
+++ TestLab/static/js/post-to-server.js
@@ -1,119 +1,123 @@
-function checkForAjaxSupport() {
-	
-  document.write(supportFileAPI())
-  document.write(supportUploadProgress())
-  document.write(supportFormData())
-  return supportFileAPI() && supportUploadProgress() && supportFormData();
-  
-  // Check if the file is API supported
-  function supportFileAPI() {
-    var fileCheck = document.createElement('INPUT');
-    fileCheck.type = 'file';
-    return 'files' in fi;
-  };
-  
-  // Check if progress events are supported
-  function supportUploadProgress() {
-    var xhr = new XMLHttpRequest();
-    return !! (xhr && ('upload' in xhr) && ('onprogress' in xhr.upload));
-  };
-  
-  // Check if FormData is supported?
-  function supportFormData() {
-    return !! window.FormData;
-  }
-}window.onload = checkForAjaxSupport();
+(function (GLOBAL, $) {
 
-// Confirm Ajax support
-if (checkForAjaxSupport()) {
-	
-  // Ajax uploads are supported
-  // Change the support message and enable the upload button
-  var notice = document.getElementById('support-notice');
-  var uploadBtn = document.getElementById('upload-button-id');
-  
-  notice.innerHTML = "";
-  uploadBtn.removeAttribute('disabled');
-  
-  // Initialize the file upload
-  initFileAjaxUpload();
-  
-}
-
-function initFileAjaxUpload() {
-	
-  var uploadBtn = document.getElementById('upload-button-id');
-  
-  uploadBtn.onclick = function (evt) {
+	function checkForAjaxSupport() {
+		
+	  document.write(supportFileAPI())
+	  document.write(supportUploadProgress())
+	  document.write(supportFormData())
+	  return supportFileAPI() && supportUploadProgress() && supportFormData();
 	  
-    var formData = new FormData();
-    
-    var destinationURI = 'http://104.196.120.212:8080/data/pvw/data/';
-    
-    // FAppend the file to FormData
-    var fileInput = document.getElementById('file-id');
-    var file = fileInput.files[0];
-    formData.append('my-file', file);
-    
-    // Send XMLHttpRequest 
-    sendXHRequest(formData, destinationURI);
-  }
-}
-
-// Once the FormData instance is ready and we know
-// where to send the data
-function sendXHRequest(formData, uri) {
+	  // Check if the file is API supported
+	  function supportFileAPI() {
+	    var fileCheck = document.createElement('INPUT');
+	    fileCheck.type = 'file';
+	    return 'files' in fi;
+	  };
+	  
+	  // Check if progress events are supported
+	  function supportUploadProgress() {
+	    var xhr = new XMLHttpRequest();
+	    return !! (xhr && ('upload' in xhr) && ('onprogress' in xhr.upload));
+	  };
+	  
+	  // Check if FormData is supported?
+	  function supportFormData() {
+	    return !! window.FormData;
+	  }
+	}
 	
-  // Get an new XMLHttpRequest instance
-  var xhr = new XMLHttpRequest();
-  
-  // Set up events
-  xhr.upload.addEventListener('loadstart', onloadstartHandler, false);
-  xhr.upload.addEventListener('progress', onprogressHandler, false);
-  xhr.upload.addEventListener('load', onloadHandler, false);
-  xhr.addEventListener('readystatechange', onreadystatechangeHandler, false);
-  
-  // Set up request
-  xhr.open('POST', uri, true);
-  
-  // Send Request
-  xhr.send(formData);
-}
+	// Confirm Ajax support
+	if (checkForAjaxSupport()) {
+		
+	  // Ajax uploads are supported
+	  // Change the support message and enable the upload button
+	  var notice = document.getElementById('support-notice');
+	  var uploadBtn = document.getElementById('upload-button-id');
+	  
+	  notice.innerHTML = "";
+	  uploadBtn.removeAttribute('disabled');
+	  
+	  // Initialize the file upload
+	  initFileAjaxUpload();
+	  
+	}
+	
+	function initFileAjaxUpload() {
+		
+	  var uploadBtn = document.getElementById('upload-button-id');
+	  
+	  uploadBtn.onclick = function (evt) {
+		  
+	    var formData = new FormData();
+	    
+	    var destinationURI = 'http://104.196.120.212:8080/data/pvw/data/';
+	    
+	    // FAppend the file to FormData
+	    var fileInput = document.getElementById('file-id');
+	    var file = fileInput.files[0];
+	    formData.append('my-file', file);
+	    
+	    // Send XMLHttpRequest 
+	    sendXHRequest(formData, destinationURI);
+	  }
+	}
+	
+	// Once the FormData instance is ready and we know
+	// where to send the data
+	function sendXHRequest(formData, uri) {
+		
+	  // Get an new XMLHttpRequest instance
+	  var xhr = new XMLHttpRequest();
+	  
+	  // Set up events
+	  xhr.upload.addEventListener('loadstart', onloadstartHandler, false);
+	  xhr.upload.addEventListener('progress', onprogressHandler, false);
+	  xhr.upload.addEventListener('load', onloadHandler, false);
+	  xhr.addEventListener('readystatechange', onreadystatechangeHandler, false);
+	  
+	  // Set up request
+	  xhr.open('POST', uri, true);
+	  
+	  // Send Request
+	  xhr.send(formData);
+	}
+	
+	// Handle the start of the transmission
+	function onloadstartHandler(evt) {
+	  var div = document.getElementById('upload-status');
+	  div.innerHTML = 'Upload started.';
+	}
+	
+	// Handle the end of the transmission
+	function onloadHandler(evt) {
+	  var div = document.getElementById('upload-status');
+	  div.innerHTML += '<' + 'br>File uploaded. Waiting for response.';
+	}
+	
+	// Handle the progress
+	function onprogressHandler(evt) {
+	  var div = document.getElementById('progress');
+	  var percent = evt.loaded/evt.total*100;
+	  div.innerHTML = 'Progress: ' + percent + '%';
+	}
+	
+	// Handle the response from the server
+	function onreadystatechangeHandler(evt) {
+	  var status, text, readyState;
+	  try {
+	    readyState = evt.target.readyState;
+	    text = evt.target.responseText;
+	    status = evt.target.status;
+	  }
+	  catch(e) {
+	    return;
+	  }
+	  if (readyState == 4 && status == '200' && evt.target.responseText) {
+	    var status = document.getElementById('upload-status');
+	    status.innerHTML += '<' + 'br>Success!';
+	    var result = document.getElementById('result');
+	    result.innerHTML = '<p>The server saw it as:</p><pre>' + evt.target.responseText + '</pre>';
+	  }
+	}
 
-// Handle the start of the transmission
-function onloadstartHandler(evt) {
-  var div = document.getElementById('upload-status');
-  div.innerHTML = 'Upload started.';
-}
-
-// Handle the end of the transmission
-function onloadHandler(evt) {
-  var div = document.getElementById('upload-status');
-  div.innerHTML += '<' + 'br>File uploaded. Waiting for response.';
-}
-
-// Handle the progress
-function onprogressHandler(evt) {
-  var div = document.getElementById('progress');
-  var percent = evt.loaded/evt.total*100;
-  div.innerHTML = 'Progress: ' + percent + '%';
-}
-
-// Handle the response from the server
-function onreadystatechangeHandler(evt) {
-  var status, text, readyState;
-  try {
-    readyState = evt.target.readyState;
-    text = evt.target.responseText;
-    status = evt.target.status;
-  }
-  catch(e) {
-    return;
-  }
-  if (readyState == 4 && status == '200' && evt.target.responseText) {
-    var status = document.getElementById('upload-status');
-    status.innerHTML += '<' + 'br>Success!';
-    var result = document.getElementById('result');
-    result.innerHTML = '<p>The server saw it as:</p><pre>' + evt.target.responseText + '</pre>';
-  }
-}
+}(window, jQuery));
