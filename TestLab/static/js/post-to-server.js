cfc9926f9bd27cc1553af94c3aedec5125043bfc
dspring86 <dspring@equityeng.com>
2016-03-17 08:06:36 -0400

    testing xmlrequest version

--- TestLab/static/js/post-to-server.js
+++ TestLab/static/js/post-to-server.js
@@ -54,7 +54,7 @@
 	    var fileInput = document.getElementById('file-id');
 	    var file_data = fileInput.files[0];
 	    formData.append('file', file_data);   
-	    
+	    /*
 	    $.ajax({
 	        url: destinationURI,
 	        type: "POST",
@@ -69,15 +69,16 @@
 	        	alert("failed");
 	        }
 	     });
+	     */
 	    
 	     
 	    
 	    // Send XMLHttpRequest 
-	    //sendXHRequest(formData, destinationURI);
+	    sendXHRequest(formData, destinationURI);
 	  }
 	}
 	
-	/*
+	
 	// Once the FormData instance is ready and we know
 	// where to send the data
 	function sendXHRequest(formData, uri) {
@@ -133,7 +134,7 @@
 	    status.innerHTML += '<' + 'br>Success!';
 	  } 
 	}
-	*/
+	
 	
 
 }(window, jQuery));
