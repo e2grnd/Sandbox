758967e2b8f425767fcf7ecb5207cd13e52d5029
dspring86 <dspring@equityeng.com>
2016-03-23 12:46:08 -0400

    Changed indexViz to embedViz

--- TestLab/views.py
+++ TestLab/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'TestLab/index_VTK.html', {})
+    return render(request, 'TestLab/index.html', {})
 
 def embedPRVW(request):
     return render(request, 'TestLab/embed.html', {})
 
-def testembedViz(request):
-    return render(request, 'TestLab/indexViz.html', {})
+def embedViz(request):
+    return render(request, 'TestLab/embedViz.html', {})
