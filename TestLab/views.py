f487c1ba3a1c1ac5aedb287dd6735509abfeff8b
dspring86 <dspring@equityeng.com>
2016-03-23 13:38:46 -0400

    Testing

--- TestLab/views.py
+++ TestLab/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'TestLab/index.html', {})
+    return render(request, 'TestLab/indexVTK.html', {})
 
 def embedViz(request):
     return render(request, 'TestLab/embedViz.html', {})
