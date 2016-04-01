9b78ea6d57d1bc0a1e3982afd834db82d7358bb4
dspring86 <dspring@equityeng.com>
2016-04-01 12:09:33 -0400

    Testing

--- static/js/post-to-server.js
+++ static/js/post-to-server.js
@@ -34,7 +34,7 @@
 	  var notice = document.getElementById("support-notice");
 	  var uploadBtn = document.getElementById('upload-button-id');
 	  
-	  notice.innerHTML = "Upload VTK File to Server";
+	  notice.innerHTML = "Upload VTK/EX2 File to Server";
 	  uploadBtn.removeAttribute('disabled');
 	  
 	  // Initialize the file upload
