d57080b2938945bdacbcf9bb9ffbc1ced0690ebe
dspring86 <dspring@equityeng.com>
2016-04-01 08:25:30 -0400

    Testing

--- static/js/paraview.ui.proxy.editor.js
+++ static/js/paraview.ui.proxy.editor.js
@@ -427,7 +427,7 @@
                                 }
                             }
                         }
-                        document.write(arrayOptions)
+                        //document.write(arrayOptions)
 
                         // => Components
                         var internalCount = range.length;
@@ -450,7 +450,7 @@
                                 .replace(/VALUE/g, paletteList[i])
                                 .replace(/LABEL/g, paletteList[i]));
                         }
-                        document.write(paletteOptions)
+                        //document.write(paletteOptions)
 
                         // ID, ARRAY_OPTIONS, COMPONENT_OPTIONS, PALETTE_OPTIONS
                         bufferProperties.push(TEMPLATE_COLOR_BY_PANEL
