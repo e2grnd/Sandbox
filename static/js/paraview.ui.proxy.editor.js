f70e3dd812604df32bc198dea871652a5f94118a
dspring86 <dspring@equityeng.com>
2016-04-04 13:17:09 -0400

    Testing

--- static/js/paraview.ui.proxy.editor.js
+++ static/js/paraview.ui.proxy.editor.js
@@ -699,7 +699,6 @@
                             id: target_container.parent().attr('data-proxy-id'),
                             colorBy: extractColorBy()
                         });
-                        document.write(event)
                         me.unbind('update-scalar-range-values').bind('update-scalar-range-values', function(newRange) {
                             $('.scalar-range-min', me).val(newRange.min);
                             $('.scalar-range-max', me).val(newRange.max);
