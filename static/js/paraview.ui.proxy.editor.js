2e39868983246e25d29b174afbe814d5f3148557
dspring86 <dspring@equityeng.com>
2016-04-04 14:59:28 -0400

    Testing

--- static/js/paraview.ui.proxy.editor.js
+++ static/js/paraview.ui.proxy.editor.js
@@ -699,16 +699,16 @@
                             id: target_container.parent().attr('data-proxy-id'),
                             colorBy: extractColorBy()
                         });
-                        //me.unbind('update-scalar-range-values').bind('update-scalar-range-values', function(newRange) {
-                        //    $('.scalar-range-min', me).val(newRange.min);
-                        //    $('.scalar-range-max', me).val(newRange.max);
-                        //});
-                        //me.unbind('notify-new-rgb-points-received').bind('notify-new-rgb-points-received', function(event) {
-                        //    $('.color-editor-container', me).trigger({
-                        //        type: 'new-rgb-points-received',
-                        //        rgbpoints: event.rgbpoints
-                        //    });
-                        //});
+                        me.unbind('update-scalar-range-values').bind('update-scalar-range-values', function(newRange) {
+                            $('.scalar-range-min', me).val(newRange.min);
+                            $('.scalar-range-max', me).val(newRange.max);
+                        });
+                        me.unbind('notify-new-rgb-points-received').bind('notify-new-rgb-points-received', function(event) {
+                            $('.color-editor-container', me).trigger({
+                                type: 'new-rgb-points-received',
+                                rgbpoints: event.rgbpoints
+                            });
+                        });
                     } else if (action === 'rescale-to-time') {
                         me.trigger({
                             type: 'rescale-transfer-function',
