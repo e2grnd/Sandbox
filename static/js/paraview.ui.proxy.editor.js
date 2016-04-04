d61db7b368103ed3097373d289fa1956c70b927f
dspring86 <dspring@equityeng.com>
2016-04-04 11:25:12 -0400

    Testing

--- static/js/paraview.ui.proxy.editor.js
+++ static/js/paraview.ui.proxy.editor.js
@@ -396,10 +396,9 @@
             if (wantColorManagement === true) {
                 // Listen to event asking me to update the scalar range
             	if(InitialApplyLegend){
-            		document.write('here')
 	            	me.unbind('update-scalar-range-values').bind('update-scalar-range-values', function(newRange) {
-	                    $('.scalar-range-min', me).val('80');
-	                    $('.scalar-range-max', me).val('1800');
+	                    $('.scalar-range-min', me).val(80);
+	                    $('.scalar-range-max', me).val(1800);
 	                });
 	            	InitialApplyLegend = 0;
             	}
