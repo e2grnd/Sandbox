1c34102dd2337439c36866739d5eb87ccc0d098e
dspring86 <dspring@equityeng.com>
2016-04-04 13:24:47 -0400

    Tetsing

--- static/js/paraview.ui.proxy.editor.js
+++ static/js/paraview.ui.proxy.editor.js
@@ -783,6 +783,7 @@
             	document.write(el)
             	var evObj = document.createEvent('Events');
       	        evObj.initEvent('click', true, false);
+      	        el.dispatchEvent(evObj);
       	        document.write(evObj.target)
       	        //el.dispatchEvent(evObj);
             	InitialApplyLegend = 0;
