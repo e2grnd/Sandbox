06eca2c55647050dcb471b4695a9795e98a6897e
dspring86 <dspring@equityeng.com>
2016-04-05 14:05:41 -0400

    Tetsing initial zoom

--- static/js/vtkweb-all.js
+++ static/js/vtkweb-all.js
@@ -3865,7 +3865,7 @@
                         otherCamera.push(renderer.camera);
                     }
                     
-                    renderer.camera.zoom(0.05)//ADDED BY DAN
+                    renderer.camera.zoom(0.1)//ADDED BY DAN
                 }
                 background = buildBackground(gl, bgColor1, bgColor2);
 
