99ab2ac184519a352479913090ad95d70ddd4437
dspring86 <dspring@equityeng.com>
2016-04-08 13:01:42 -0400

    testing

--- static/js/main.js
+++ static/js/main.js
@@ -711,7 +711,7 @@
                     viewVal, pipelineDataModel.view.ui, "_View"
                     );
 
-               document.write(options.widgetKey)
+               document.write(options.widgetSettings)
             if (!$.isEmptyObject(colorBy) && colorBy.hasOwnProperty('array')) {
                 props = [].concat(colMgmtVal,
                                   extractRepresentation(pipelineDataModel.representation.properties),
