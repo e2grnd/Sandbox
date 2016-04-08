c6861e2331cbdd59cb237e4b73a973599ab4fa87
dspring86 <dspring@equityeng.com>
2016-04-08 12:59:36 -0400

    Testing print leaf

--- static/js/main.js
+++ static/js/main.js
@@ -711,7 +711,7 @@
                     viewVal, pipelineDataModel.view.ui, "_View"
                     );
 
-               document.write(pipelineDataModel.metadata.name)
+               document.write(pipelineDataModel.metadata.leaf)
             if (!$.isEmptyObject(colorBy) && colorBy.hasOwnProperty('array')) {
                 props = [].concat(colMgmtVal,
                                   extractRepresentation(pipelineDataModel.representation.properties),
