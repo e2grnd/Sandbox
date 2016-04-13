e2ff6f6272dc54dc4af0dde472fbc494d00b32ec
dspring86 <dspring@equityeng.com>
2016-04-13 11:12:47 -0400

    Testing

--- static/js/vtkweb-all.js
+++ static/js/vtkweb-all.js
@@ -2453,7 +2453,7 @@
             fp_ = cam.focalPoint(),
             up_ = cam.viewUpDirection(),
             pos_ = cam.position(),
-            fp = [fp_[0], fp_[1], fp_[2]],
+            fp = [fp_[0]*2, fp_[1]*2, fp_[2]*2],
             up = [up_[0], up_[1], up_[2]],
             pos = [pos_[0], pos_[1], pos_[2]];
         m_session.call("viewport.camera.update", [Number(m_options.view), fp, up, pos]);
