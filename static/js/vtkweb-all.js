dcf50dde749bdb020ad45b2b1d5bf807786ce80f
dspring86 <dspring@equityeng.com>
2016-04-15 16:08:59 -0400

    Testing

--- static/js/vtkweb-all.js
+++ static/js/vtkweb-all.js
@@ -3864,9 +3864,6 @@
                     if(renderer.layer === 0) {
                         cameraLayerZero = renderer.camera;
                         bgColor1 = bgColor2 = renderer.Background1; //THIS IS WHERE THE BACKGROUND COLOR IS SET
-                        bgColor1 = [0.48,0.49,0.59];
-                        bgColor2 = [0.48,0.49,0.59];
-                        //document.write(bgColor1[0],"   ",bgColor1[1],"   ",bgColor1[2])
                         if(typeof(renderer.Background2) != "undefined") {
                             bgColor2 = renderer.Background2;
                         }
