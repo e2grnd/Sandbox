07a8f9a7e8d5943731c82d06e1c1810f0f36872b
dspring86 <dspring@equityeng.com>
2016-04-18 10:51:51 -0400

    Remove session call in resetCamera Function

--- static/js/main.js
+++ static/js/main.js
@@ -420,7 +420,6 @@
     function resetCamera() {
         if(viewport) {
             viewport.resetCamera();
-            session.call("viewport.camera.zoomonload", [-1, 0.05]);
         }
     }
 
