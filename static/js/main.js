5f56c4a0a86ed72e765e72246a8ef433a15fdf30
dspring86 <dspring@equityeng.com>
2016-10-24 16:32:11 -0400

    testing

--- static/js/main.js
+++ static/js/main.js
@@ -570,16 +570,24 @@
 
     function onProxyApply(event) {
         startWorking();
-        session.call('pv.proxy.manager.update', [event.properties]).then(invalidatePipeline, invalidatePipeline);
-        // Args: representation, colorMode, arrayLocation='POINTS', arrayName='', vectorMode='Magnitude', vectorComponent = 0, rescale=False
-        var args = [].concat(event.colorBy.representation, event.colorBy.mode, event.colorBy.array, event.colorBy.component);
-        startWorking();
-        session.call('pv.color.manager.color.by', args).then(invalidatePipeline, error);
-        // Update palette ?
-        if(event.colorBy.palette) {
+        session.call('pv.color.manager.rgb.points.get', [event.colorBy.array[2]]).then(function(result) {
+        	session.call('pv.proxy.manager.update', [event.properties]).then(invalidatePipeline, invalidatePipeline);
+            // Args: representation, colorMode, arrayLocation='POINTS', arrayName='', vectorMode='Magnitude', vectorComponent = 0, rescale=False
+            var args = [].concat(event.colorBy.representation, event.colorBy.mode, event.colorBy.array, event.colorBy.component);
             startWorking();
-            session.call('pv.color.manager.select.preset', [ event.colorBy.representation, event.colorBy.palette ]).then(invalidatePipeline, error);
-        }
+            session.call('pv.color.manager.color.by', args).then(invalidatePipeline, error);
+            // Update palette ?
+            if(event.colorBy.palette) {
+                startWorking();
+                session.call('pv.color.manager.select.preset', [ event.colorBy.representation, event.colorBy.palette ]).then(invalidatePipeline, error);
+            }
+            proxyEditor.trigger({
+                type: 'update-rgb-points',
+                colorBy: event.colorBy,
+                rgbInfo: result
+            });
+        }, error);
+        
     }
 
     // ------------------------------------------------------------------------
