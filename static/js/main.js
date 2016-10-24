acd182f1d9eb318d53f059123a515b09a2604615
dspring86 <dspring@equityeng.com>
2016-10-24 15:30:59 -0400

    testing

--- static/js/main.js
+++ static/js/main.js
@@ -579,14 +579,16 @@
         var rgbStored = {};
         // Update palette ?
         session.call('pv.color.manager.rgb.points.get', [event.colorBy.array[1]]).then(function(result) {
-        	if(event.colorBy.palette) {
-                startWorking();
-                session.call('pv.color.manager.select.preset', [ event.colorBy.representation, event.colorBy.palette ]).then(invalidatePipeline, error);
-            }
+        	
             proxyEditor.trigger({
                 'type': 'notify-new-rgb-points-received',
                 'rgbpoints': result
             });
+            
+            if(event.colorBy.palette) {
+                startWorking();
+                session.call('pv.color.manager.select.preset', [ event.colorBy.representation, event.colorBy.palette ]).then(invalidatePipeline, error);
+            }
             workDone();
         }, error);
         console.log("Here")
