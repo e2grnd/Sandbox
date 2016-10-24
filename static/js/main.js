2a7aa356c2ed403bc3304393cb98a776142b78da
dspring86 <dspring@equityeng.com>
2016-10-24 12:02:34 -0400

    Returned to image rendering

--- static/js/main.js
+++ static/js/main.js
@@ -406,7 +406,7 @@
         $(viewportSelector).empty()
                            .bind('captured-screenshot-ready', onScreenshotCaptured);
         viewport = vtkWeb.createViewport({session: session});
-        viewport.setActiveRenderer('vgl');
+        viewport.setActiveRenderer('image');
         viewport.bind(viewportSelector);
     }
 
